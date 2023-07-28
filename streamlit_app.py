72c16891e7b391d1872ddef2c0c794acae1675f3
njakhmola <jakhmolanidhi22@gmail.com>
2023-07-28 12:46:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-range Egg")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
